b1f08a774680af6969aea3a5ecc642ce0630f990
Sanju Devadas <sanjudevadas11@gmail.com>
2024-08-04 20:05:26 +0530

    editing the props

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,15 +3,21 @@ import RestaurentCard from "./RestaurentCard";
 const Body = () => {
   return (
     <div className="body-container">
-      <div className="search">
-Search
-      </div>
+      <div className="search">Search</div>
 
       <div className="res-container">
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
+        <RestaurentCard
+          resName="Meghna Foods"
+          cuisine="North Indian Biriyani"
+        />
+          <RestaurentCard
+          resName="KFC"
+          cuisine="Burger"
+        />
+          <RestaurentCard
+          resName="Nahdi"
+          cuisine="Mandhi"
+        />
       </div>
     </div>
   );
--- src/components/RestaurentCard.jsx
+++ src/components/RestaurentCard.jsx
@@ -1,14 +1,19 @@
 import food1 from './food1.jpg'
+import PropTypes from 'prop-types'
 
-const RestaurentCard = () => {
+const RestaurentCard = (props) => {
   return (
     <div className="res-card">
     <img src={food1} alt="" />
-    <h3>Meghna Foods</h3>
-    <h4>North Indian Biriyani</h4>
+    <h3>{props.resName}</h3>
+    <h4>{props.cuisine}</h4>
     <h5>⭐4.3</h5>
     </div>
   )
 }
+RestaurentCard.propTypes = {
+    resName: PropTypes.string.isRequired,
+    cuisine: PropTypes.string.isRequired,
+  };
 
 export default RestaurentCard

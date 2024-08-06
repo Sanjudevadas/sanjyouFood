98d93808e4a411470035bf99a4d9ad0c58d04259
Sanju Devadas <sanjudevadas11@gmail.com>
2024-08-06 22:35:00 +0530

    studied usestate

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,23 +1,29 @@
 import RestaurentCard from "./RestaurentCard";
-
+//import { useState } from "react";
 const Body = () => {
   return (
     <div className="body-container">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          onClick={() => {
+            console.log("Button Clicked");
+          }}
+          className="filter-btn"
+          onMouseOver={() => {
+            console.log("Mouse over");
+          }}
+        >
+          Top rated Restaurents
+        </button>
+      </div>
 
       <div className="res-container">
         <RestaurentCard
           resName="Meghna Foods"
           cuisine="North Indian Biriyani"
         />
-          <RestaurentCard
-          resName="KFC"
-          cuisine="Burger"
-        />
-          <RestaurentCard
-          resName="Nahdi"
-          cuisine="Mandhi"
-        />
+        <RestaurentCard resName="KFC" cuisine="Burger" />
+        <RestaurentCard resName="Nahdi" cuisine="Mandhi" />
       </div>
     </div>
   );

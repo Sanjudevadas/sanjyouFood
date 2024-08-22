173daaf143010dfc83b7d49e25b1b344a752c6b4
Sanju Devadas <sanjudevadas11@gmail.com>
2024-08-22 20:58:43 +0530

    added Routing to about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import Header from "./components/Header"
-import Body from "./components/Body"
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import Body from "./components/Body";
+import About from "./components/About";
 
 function App() {
- 
   return (
-   <div className="app">
-   <Header/>
-   <Body/>
-   </div>
-  )
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
